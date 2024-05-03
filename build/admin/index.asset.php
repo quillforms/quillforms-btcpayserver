2a8253d969f9d967a6d02d64b35461e537da1678
aaharidy <abdoharidy72@gmail.com>
2024-05-03 23:18:44 +0300

    build and pot

---
+++ build/admin/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('emotion', 'quillforms-admin-components', 'quillforms-config', 'quillforms-navigation', 'react', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'b2961bfe5aabf30f1b13');
---
+++ build/renderer/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('emotion', 'quillforms-config', 'quillforms-renderer-core', 'react', 'wp-element', 'wp-hooks'), 'version' => 'f400f10837c4580aaf7e');
